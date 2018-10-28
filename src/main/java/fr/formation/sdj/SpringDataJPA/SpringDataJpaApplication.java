f64ac56382891cf1aaa665b37ad06b118fe97bd0
Dalencourt Alex <alex.dalencourt@yahoo.fr>
2018-10-28 13:08:07 +0100

    Init auto conf database H2

--- src/main/java/fr/formation/sdj/SpringDataJPA/SpringDataJpaApplication.java
+++ src/main/java/fr/formation/sdj/SpringDataJPA/SpringDataJpaApplication.java
@@ -1,9 +1,13 @@
 package fr.formation.sdj.SpringDataJPA;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"fr.formation.sdj.entities"})
 public class SpringDataJpaApplication {
 
 	public static void main(String[] args) {
