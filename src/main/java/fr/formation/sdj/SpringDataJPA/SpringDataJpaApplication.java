c6b8a5b92b7ceff63817c9138b8ee197a28a9f14
Dalencourt Alex <alex.dalencourt@yahoo.fr>
2018-10-30 14:52:06 +0100

    Repository scan

--- src/main/java/fr/formation/sdj/SpringDataJPA/SpringDataJpaApplication.java
+++ src/main/java/fr/formation/sdj/SpringDataJPA/SpringDataJpaApplication.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @EnableAutoConfiguration
 @EntityScan(basePackages = {"fr.formation.sdj.entities"})
-@ComponentScan(basePackages = {"fr.formation.sdj.controller"})
+@ComponentScan(basePackages = {"fr.formation.sdj.controller","fr.formation.sdj.repositories"})
 public class SpringDataJpaApplication {
 
 	public static void main(String[] args) {
