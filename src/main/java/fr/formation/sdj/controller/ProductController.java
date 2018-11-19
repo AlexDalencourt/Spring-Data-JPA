1a4ec73e07864a249d5f8008a212c29d72ca9d8e
Dalencourt Alex <alex.dalencourt@yahoo.fr>
2018-11-19 21:25:10 +0100

    Filter

--- src/main/java/fr/formation/sdj/controller/ProductController.java
+++ src/main/java/fr/formation/sdj/controller/ProductController.java
@@ -1,38 +1,40 @@
 package fr.formation.sdj.controller;
 
+import fr.formation.sdj.entities.Product;
 import fr.formation.sdj.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-//@RequestMapping("/")
+@RequestMapping("/")
 public class ProductController {
 
     @Autowired
     private ProductRepository productRepository;
 
-    //    @RequestMapping(method = RequestMethod.GET)
-    @RequestMapping("/")
+    @RequestMapping(method = RequestMethod.GET)
     public String home(final ModelMap model) {
         model.put("productList", productRepository.findAll());
-//        model.put("productRef", model.get("productList"));
-//        model.put("productFilter", new Product());
+        model.put("productRef", model.get("productList"));
+        model.put("productFilter", new Product());
         return "home";
     }
 
-//    @RequestMapping(method = RequestMethod.POST)
-//    public String filter(@ModelAttribute final Product product, final ModelMap model) {
-//        model.put("productRef", productRepository.findAll());
-//        if (product.getName() == null) {
-//            model.put("productList", model.get("productRef"));
-//        } else {
-//            model.put("productList", productRepository.findAllByName(product.getName()));
-//        }
-//        model.put("productFilter", new Product());
-//        return "home";
-//    }
+    @RequestMapping(method = RequestMethod.POST)
+    public String filter(@ModelAttribute final Product product, final ModelMap model) {
+        model.put("productRef", productRepository.findAll());
+        if (product.getName() == null) {
+            model.put("productList", model.get("productRef"));
+        } else {
+            model.put("productList", productRepository.findAllByName(product.getName()));
+        }
+        model.put("productFilter", new Product());
+        return "home";
+    }
 
     public ProductRepository getProductRepository() {
         return productRepository;
--- src/main/java/fr/formation/sdj/repositories/ProductRepository.java
+++ src/main/java/fr/formation/sdj/repositories/ProductRepository.java
@@ -3,7 +3,9 @@ package fr.formation.sdj.repositories;
 import fr.formation.sdj.entities.Product;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends CrudRepository<Product, Integer> {
 
-//    List<Product> findAllByName(String name);
+    List<Product> findAllByName(String name);
 }
